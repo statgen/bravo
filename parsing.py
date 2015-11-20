0103f0f02073f3ab1675481039a67bdd74453da5
Daniel Taliun <dtaliun@inpsyght.sph.umich.edu>
2015-11-20 18:36:41 -0500

    Added CADD scores and rsIds from VEP

--- parsing.py
+++ parsing.py
@@ -101,7 +101,19 @@ def get_variants_from_sites_vcf(sites_vcf):
                 variant = {}
                 variant['chrom'] = fields[0]
                 variant['pos'] = pos
-                variant['rsid'] = fields[2]
+
+                #DT: We will take rsIds and CADD from the VEP annotation
+                #variant['rsid'] = fields[2]
+
+                if vep_annotations:
+                   variant['rsid'] = vep_annotations[0]['Existing_variation'].split('&')
+                   variant['cadd_raw'] = None if not vep_annotations[0]['CADD_RAW'] else float(vep_annotations[0]['CADD_RAW']) 
+                   variant['cadd_phred'] = None if not vep_annotations[0]['CADD_PHRED'] else float(vep_annotations[0]['CADD_PHRED'])
+                else:
+                   variant['rsid'] = []
+                   variant['cadd_raw'] = None
+                   variant['cadd_phred'] = None
+                   
                 variant['xpos'] = get_xpos(variant['chrom'], variant['pos'])
                 variant['ref'] = ref
                 variant['alt'] = alt
@@ -150,7 +162,7 @@ def get_variants_from_sites_vcf(sites_vcf):
                 # DT:    variant['genotype_qualities'] = [zip(gq_mids, map(int, x.split('|'))) for x in hists_all]
                 # DT: dirty temporary fill-in:
                 variant['genotype_depths'] = [zip(dp_mids, map(int, x.split('|'))) for x in hists_all]
-                variant['genotype_qualities'] = [zip(gq_mids, map(int, x.split('|'))) for x in hists_all]
+                variant['genotype_qualities'] = [zip(gq_mids, map(int, x.split('|'))) for x in hists_all] 
 
                 yield variant
         except Exception:
