5d7448e8ab5fdee0e782283f32d977ade854850b
Peter VandeHaar <pjvandehaar@gmail.com>
2016-10-17 09:52:42 -0400

    Update for Bravo Freeze3 Public

--- exac.py
+++ exac.py
@@ -29,7 +29,7 @@ import functools
 import contextlib
 
 app = Flask(__name__)
-app.config.from_object('flask_config.BravoFreeze3Config')
+app.config.from_object('flask_config.BravoFreeze3PublicConfig')
 mail_on_500(app, app.config['ADMINS'])
 Compress(app)
 
