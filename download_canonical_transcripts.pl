20b04b3c4fed7f047ae3ad4889764ebf2ff99fd9
Daniel Taliun <dtaliun@browser.sph.umich.edu>
2017-07-21 13:31:31 -0400

    script to obtain canonical transcripts

---
+++ download_canonical_transcripts.pl
@@ -0,0 +1,41 @@
+use Bio::EnsEMBL::Registry;
+
+my $registry = 'Bio::EnsEMBL::Registry';
+
+=begin listdb
+
+$registry->load_registry_from_db(
+   -host => 'ensembldb.ensembl.org',
+   -user => 'anonymous'
+);
+
+my @db_adaptors = @{ $registry->get_all_DBAdaptors() };
+
+foreach my $db_adaptor (@db_adaptors) {
+   my $db_connection = $db_adaptor->dbc();
+
+   printf(
+      "species/group\t%s/%s\ndatabase\t%s\nhost:port\t%s:%s\n\n",
+      $db_adaptor->species(), $db_adaptor->group(),
+      $db_connection->dbname(), $db_connection->host(),
+      $db_connection->port()
+   );
+}
+
+=end listdb
+
+=cut
+
+
+$registry->load_registry_from_db(
+   -host => 'ensembldb.ensembl.org',
+   -user => 'anonymous',
+   -dbname => 'homo_sapiens_core_89_38'
+); 
+
+my $genes_adaptor = $registry->get_adaptor('human', 'core', 'gene');
+my @genes = @{ $genes_adaptor->fetch_all };
+
+foreach my $gene (@genes) {
+   printf("%s %s\n", $gene->stable_id, $gene->canonical_transcript->stable_id)
+}
