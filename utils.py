10f62d2b0a28d76bb72bfced4515704c285d7978
Daniel Taliun <dtaliun@browser.sph.umich.edu>
2017-07-24 10:33:53 -0400

    added traceback

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from collections import OrderedDict
 from operator import itemgetter
+import traceback
 
 AF_BUCKETS = [0.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1]
 METRICS = [
