c64a1726f684ecbb3b5b83295b0cf6b0254fd3ab
Peter VandeHaar <pjvandehaar@gmail.com>
2017-08-29 15:32:20 -0400

    order by [csq, cadd]

--- static/gene.js
+++ static/gene.js
@@ -454,7 +454,10 @@ function create_variant_table() {
             },
         },
 
-        order: [[columns.map(function(d){return d.name==='csq'}).indexOf(true), 'asc']],
+        order: [
+            [columns.map(function(d){return d.name==='csq'}).indexOf(true), 'asc'],
+            [columns.map(function(d){return d.name==='cadd_phred'}).indexOf(true), 'desc'],
+        ],
         columns: columns,
 
         dom: '<ipl>rft', // default is 'lfrtip'.  l=length f=filtering t=table i=info p=paging, r=processing
