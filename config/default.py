75eacb41571db9945d60aef547e5e8824890d082
Jacob Pleiness <pleiness@umich.edu>
2018-08-20 13:33:20 -0400

    Change data dirs, default to no proxy, mongo host
    - Simplify data dirs at the /data/ dir
    - Change default to be configured for no proxy
    - Host for mongodb will be on local docker network with mongo hostname

--- config/default.py
+++ config/default.py
@@ -1,6 +1,6 @@
 DEBUG = False
 TESTING = False
-PROXY = True # True if app is proxied by Apache or similar. 
+PROXY = False # True if app is proxied by Apache or similar. 
 
 BROWSER_NAME = 'Bravo'
 DATASET_NAME = 'Example Dataset'
@@ -9,12 +9,12 @@ NUM_SAMPLES = 0
 NUM_VARIANTS = 'XYZ million'
 
 MONGO = {
-    'host': 'localhost',
+    'host': 'mongo',
     'port': 27017,
-    'name': 'example'
+    'name': 'bravo'
 }
 DOWNLOAD_ALL_FILEPATH = ''
-URL_PREFIX = '/dev'
+URL_PREFIX = ''
 
 GOOGLE_ANALYTICS_TRACKING_ID = ''
 SECRET_KEY = ''
@@ -39,10 +39,11 @@ BRAVO_AUTH_SECRET = ''
 BRAVO_ACCESS_SECRET = ''
 BRAVO_AUTH_URL_PREFIX = '/api/' + API_VERSION + '/auth'
 
-IGV_REFERENCE_PATH = '/var/bravo/data/genomes/'
-IGV_CRAM_DIRECTORY = '/var/bravo/data/cram/'
+IGV_REFERENCE_PATH = '/data/genomes/'
+IGV_CRAM_DIRECTORY = 'data/cram/'
 IGV_CACHE_COLLECTION = 'igv_cache'
-IGV_CACHE_DIRECTORY = '/var/bravo/cache/igv_cache/'
+IGV_CACHE_DIRECTORY = '/data/cache/igv_cache/'
+BASE_COVERAGE_DIRECTORY = '/var/bravo/data/coverage/'
 IGV_CACHE_LIMIT = 1000
 
 ADMINS = [
@@ -52,4 +53,3 @@ ADMINS = [
 ADMIN = False # True if app is running in admin mode.
 ADMIN_ALLOWED_IP = []
 
-BASE_COVERAGE_DIRECTORY = '/var/bravo/data/coverage/'
