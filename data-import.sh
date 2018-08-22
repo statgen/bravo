dbbd5ddb15ca5a61dcc02c994f00a3a238623003
Jacob Pleiness <jacobpleiness@mars.ddns.sph.umich.edu>
2018-08-22 15:32:05 -0400

    Add description of args to comments

--- data-import.sh
+++ data-import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Build tools and load data into MongoDB. Insert proper container name
+# Build tools and load data into MongoDB. Insert proper container name as well as INSTALL.sh args for humane genome and threads
 docker exec bravo_web_1 /bin/bash -c /deploy/INSTALL.sh -v hg38 -t 8
 
 # Create variants collection in MongoDB.
